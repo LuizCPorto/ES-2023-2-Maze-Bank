bb846af4273afdad25a792e1e95bf462ae41743a
João Victor <joaovictoribeirosantos11@gmail.com>
2023-09-19 22:29:38 -0300

    identando

--- Model/Cadastro.php
+++ Model/Cadastro.php
@@ -1,9 +1,34 @@
 <?php
 
-require_once("../Config/Connection.php");
+require_once("./Connection.php");
 
 class Cadastro
 {
+
+    
+
+    function cadastro($username, $email, $cpf, $password): bool
+    {
+        $Conector = new Connection();
+        $conexao = $Conector->getConnection();
+
+        if ($this->usuarioNaoExisteNoBd($cpf, $email, $conexao)) {
+
+            try {
+                $conexao->query("INSERT INTO Usuarios (username, email, cpf, password) VALUES ('$username', '$email', '$cpf', '$password')");
+                $conexao = null;
+                // usuario cadastrado com sucesso
+                return true;
+            } catch (PDOException $th) {
+                echo $th->getMessage();
+            }
+
+        } else {
+            // email ou cpf ja existem
+            $conexao = null;
+            return false;
+        }
+    }
 
     private function usuarioNaoExisteNoBd($cpf, $email, PDO $conexao): bool
     {
@@ -21,24 +46,5 @@ class Cadastro
         return empty($sql);
     }
 
-    function cadastro($username, $email, $cpf, $password): bool
-    {
-        $Conector = new Connection();
-        $conexao = $Conector->getConnection();
 
-        if ($this->usuarioNaoExisteNoBd($cpf, $email, $conexao)) {
-
-            try {
-                $conexao->query("INSERT INTO Usuarios (username, email, cpf, password) VALUES ('$username', '$email', '$cpf', '$password')");
-                $conexao = null;
-                return true;
-            } catch (PDOException $th) {
-                echo $th->getMessage();
-            }
-
-        } else {
-            $conexao = null;
-            return false;
-        }
-    }
 }
