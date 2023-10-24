6fe13236bc8066dfcf93a64884e096abd31af946
Caio Santos Silva <c.santosggwp@gmail.com>
2023-10-24 20:47:58 -0300

    conexao com bd

---
+++ configuration/cfg.php
@@ -0,0 +1,8 @@
+<?php
+$dbHost = 'containers-us-west-96.railway.app';
+$dbUsername = 'root';
+$dbPassword = 'rA09CY5wO2Bkxy8FFvZV';
+$dbName = 'railway';
+$PORT = "8044";
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName,$PORT);
+?>
