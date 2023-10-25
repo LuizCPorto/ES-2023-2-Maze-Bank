7d292e4508687531e24a39f116c5c0bd27d07527
João Victor <joaovictoribeirosantos11@gmail.com>
2023-10-24 21:59:28 -0300

    Painel finalizado

--- views/painel.php
+++ views/painel.php
@@ -1,13 +1,25 @@
+<?php
+  session_start();
+
+  if (isset($_SESSION['nome_do_usuario'])) {
+      $nome_do_usuario = $_SESSION['nome_do_usuario'];
+  } else {
+      $nome_do_usuario = "Usuário não está logado!";
+  }
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/7c15533c30.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/painel.css" />
     <title>Painel</title>
   </head>
-  <body class="h-screen overflow-y-hidden bg-[#d9d9d9]">
+  <body class="h-screen bg-[#d9d9d9]">
 
     <?php
       require_once(__DIR__ ."/../controllers/painelDoCliente/controllerPainel.php");
@@ -17,18 +29,26 @@
     ?>
 
 
-    <header class="flex w-full justify-between border-b border-black p-5 px-24">
-      <img src="./img/logo.webp" alt="logo" class="my-1 w-20" />
-
-      <button class="my-2 rounded-xl bg-red-600 hover:bg-red-700 font-bold text-white">
-        <a href="../routes/home.php" class="mx-4 block h-full w-36 pt-6"
-          >Voltar para Home</a
-        >
-      </button>
+    <header>
+            <h1 class="text-[2.5rem] mb-2">MazeBank</h1>
+            <p class="mb-3">Seu Banco de Confiança</p>
+            <div class="user-info">
+                <span><?php echo $nome_do_usuario; ?></span>
+            </div>
     </header>
 
-    <main class="m-auto mt-20 h-5/6 w-8/12 rounded-xl bg-red-600 ">
-      <form action="../controllers/painelDoCliente/controllerPainel.php" method="POST" class="m-auto w-1/2 pt-24">
+    <nav>
+        <ul>
+            <li><a class="btn btn-dark" href="../routes/home.php">Início</a></li>
+            <li><a class="btn btn-dark" href="../routes/conta.html">Conta</a></li>
+            <li><a class="btn btn-dark" href="../routes/transferencias.html">Transferências</a></li>
+            <li><a class="btn btn-dark" href="../routes/configuracoes.html">Configurações</a></li>
+            <li><a class="btn btn-dark" href="../views/painel.php">Ajustes</a></li>
+        </ul>
+    </nav>
+
+    <main class="mx-auto mt-5 h-5/6 w-8/12 rounded-xl bg-[#ba2e29] ">
+      <form action="../controllers/painelDoCliente/controllerPainel.php" method="POST" class="mx-auto w-1/2 pt-16">
         <h1 class="mb-16 text-6xl font-bold text-white">Ajustes</h1>
 
         <label for="name" class="text-xl font-bold text-white"
